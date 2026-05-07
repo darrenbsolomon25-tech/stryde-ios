8617cf629555ec1da39fb8defa52f7ea7aee4fcd
Darren <darrenbsolomon25@gmail.com>
2026-05-07 16:57:12 +0300

    Initial Commit

---
+++ Stryde IOS/Stryde_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stryde_IOSApp.swift
+//  Stryde IOS
+//
+//  Created by Darren Solomon on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Stryde_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
